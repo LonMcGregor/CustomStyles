140aef0f995a7531752812615c8cd9b4a4edfb25
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2018-03-04 14:23:20 +0000

    support new twitch chat popout

--- twitch.user.js
+++ twitch.user.js
@@ -6,37 +6,41 @@
 // ==/UserScript==
 "use strict";
 
-function makeStyle(){
-    let style_text = ".theme--dark .vod-message .qa-mod-message span {color: white !important}"
-    if(window.location.href.indexOf("?no-mobile-redirect=true") >= 0 || window.location.href.indexOf("?popout") >= 0) {
-        style_text += `body.dark * {
-            background-color: black !important;
-            border-color: purple !important;
-            color: white !important;
-        }`;
-        setTimeout(function add_the_button(){
-            const chat_buttons = document.querySelector("div.js-chat-buttons.chat-buttons-container.clearfix");
-            if(chat_buttons){
-                const toggle_button = document.createElement("a");
-                toggle_button.className = "button button--icon-only float-left";
-                toggle_button.addEventListener("click", function(){
-                    document.body.className = document.body.className === "ember-application dark" ? "ember-application light" : "ember-application dark";
-                });
-                /* stole the camera svg from vivaldi */
-                toggle_button.innerHTML = `<figure id="ember1076" class="icon ember-view">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+function addToggle(){
+    const chat_buttons = document.querySelector("div.chat-input__buttons-container > div");
+    if(chat_buttons){
+        const toggle_button = document.createElement("a");
+        toggle_button.className = "button button--icon-only float-left";
+        toggle_button.addEventListener("click", function(){
+            document.body.className = document.body.className === "dark" ? "light" : "dark";
+        });
+        /* stole the camera svg from vivaldi */
+        toggle_button.innerHTML = `<button class="tw-button-icon">
+                <span class="tw-button-icon__icon">
+                    <figure class="tw-svg">
+                    <svg class="tw-svg__asset tw-svg__asset--inherit" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
                     <path d="M14 4h-2V2H4v2H0v10h16V4h-2zm0 8H2V6h4V4h4v2h4v6z"></path>
                     <circle cx="8" cy="9" r="2"></circle>
                     </svg>
-                    </figure>`;
-                chat_buttons.appendChild(toggle_button);
-            }
-            else{setTimeout(add_the_button, 500)}
-        }, 2000)
-    }
+                    </figure>
+                </span>
+            </button>`;
+        chat_buttons.appendChild(toggle_button);
+    } else { setTimeout(addToggle, 500); }
+}
+
+function makeStyle(){
     const style_element = document.createElement('style');
-    style_element.innerHTML = style_text;
+    style_element.innerHTML = `
+    body.dark {
+        filter: invert() hue-rotate(180deg);
+    }
+    body.dark img {
+        filter: invert() hue-rotate(180deg);
+    }
+        `;
     document.body.appendChild(style_element);
 }
 
 makeStyle();
+setTimeout(addToggle, 2000);
